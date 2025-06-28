0f9bf23cca4586f45fd2a0d8d213c344893b5541
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-06-28 17:55:03 +0530

    Provider of trpc is set-up

--- apps/web/providers/TrpcProvider.tsx
+++ apps/web/providers/TrpcProvider.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { FC, PropsWithChildren } from 'react'
 import { queryClient, trpc, trpcClient } from "../trpc/client";
 import { QueryClientProvider } from '@tanstack/react-query';
