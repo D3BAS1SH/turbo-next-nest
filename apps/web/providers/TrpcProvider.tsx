a5a12c95e16b205ed4b65a31c84e3fb599de9341
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-06-28 17:36:30 +0530

    Query client set-up

---
+++ apps/web/providers/TrpcProvider.tsx
@@ -0,0 +1,17 @@
+import { FC, PropsWithChildren } from 'react'
+import { queryClient, trpc, trpcClient } from "../trpc/client";
+import { QueryClientProvider } from '@tanstack/react-query';
+
+interface TrpcProviderProps extends PropsWithChildren {
+    
+}
+
+const TrpcProvider: FC<TrpcProviderProps> = ({ children }) => {
+    return <trpc.Provider client={trpcClient} queryClient={queryClient}>
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    </trpc.Provider>
+}
+
+export default TrpcProvider
